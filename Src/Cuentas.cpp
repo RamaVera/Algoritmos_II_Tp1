f2081f5a38c57cae72da54910074541807a16221
SergioDanielGalvan <55294440+SergioDanielGalvan@users.noreply.github.com>
2020-11-25 00:03:08 -0300

    Implementando los prototipos de Cuentas
    Aunque en parte estoy laburando a Ciegas.
    No conozco nada de lo que esta hecho en lo pendiente de minar de MemPool y el resto.

--- Src/Cuentas.cpp
+++ Src/Cuentas.cpp
@@ -31,3 +31,74 @@ Cuentas::~Cuentas() {
 //---Setters---//
 
 //---Otros---//
+
+
+size_t Cuentas::NuevoNumero() {
+	// Genera un automumérico.
+	static size_t Id = 0;
+	return Id++;
+}
+
+bool Cuentas::addcuenta( const std::string addr, const std::string alias, const float monto ) {
+	cuentas_t * C = NULL;
+
+	// Checks
+	if ( addr.empty() ) { 
+		return false;
+	}
+	else if ( ! BlockChainBuilder::CheckHash( addr, TiposHash::clavehash256 ) ) { 
+		return false;
+	}
+	else if ( ! BlockChainBuilder::CheckHexa( addr ) ) {
+		return false;
+	}
+
+	try {
+		C = new cuentas_t;
+		C->addr = addr;
+		C->saldo = monto;
+		C->alias = alias;
+		C->numerocuenta = Cuentas::NuevoNumero();
+		this->listadocuentas.insertar( C );
+	}
+	catch (std::bad_alloc& ba)
+	{
+		std::cerr << "bad_alloc caught: " << ba.what() << '\n';
+	}
+
+	return true;
+}
+
+bool Cuentas::addcuenta( const std::string addr, const std::string alias ) {
+	return this->addcuenta( addr, alias, 0 /* Para eliminar ambigüedades en la sobrecarga */ );
+}
+
+bool Cuentas::addcuenta( const std::string addr, const float monto ) {
+	return this->addcuenta( addr, "", monto );
+}
+
+std::string Cuentas::getalias( const std::string addr ) {
+	std::string alias = "";
+	if ( addr.empty() ) { 
+		return alias;
+	}
+	else if ( ! BlockChainBuilder::CheckHash( addr, TiposHash::clavehash256 ) ) { 
+		return alias;
+	}
+	else if ( ! BlockChainBuilder::CheckHexa( addr ) ) {
+		return alias;
+	}
+
+	if ( ! this->listadocuentas.vacia() ) {
+		lista <cuentas_t *>::iterador it( listadocuentas );
+		it = this->listadocuentas.primero();
+		do {
+			if ( it.dato()->addr == addr ) {
+				alias = it.dato()->alias;
+				break;
+			}
+			it.avanzar();
+		} while ( ! it.extremo() );
+	}
+	return alias;
+}
--- Src/Cuentas.h
+++ Src/Cuentas.h
@@ -14,6 +14,7 @@ class Cuentas {
 private:
 	lista <cuentas_t *> listadocuentas;	      	// Lista de cuentas_t
 	size_t cantidad; 							// Total de cuentas_t
+	static size_t NuevoNumero();
 
 public:
 	//---Constructores---//
@@ -41,6 +42,8 @@ public:
 	bool extraccion( const std::string addr, const float monto );
 	bool depositopendiente( const std::string addr, const float monto );
 	bool extraccionpendiente( const std::string addr, const float monto );
+	void listadototal( const float saldominimo = 0 );
+
 };
 
 #endif /* CUENTAS_H_ */
