01fa09030e554390887a942c1b9ccc02801f198a
SergioDanielGalvan <55294440+SergioDanielGalvan@users.noreply.github.com>
2020-12-03 16:07:57 -0300

    Se modifico Cuentas y BlockChainHistoryBook
    La idea es que cada método:
    
    	static void BorrarHistoria( void );
    	static bool AddBlock( Block *& B );
    	static bool AddListaBlocks( lista <Block *> & lista );
    
    dentro tenga una llamada a cuentas para que actualice saldos reales confirmados con:
    
    	void vaciarcuentas( void );
    	bool updatedatos( const Block * B );
    	bool updatedatos( lista <Block *> & lista );
    	bool updatedatos( Transaction & T );
    
     En Meempool habria que hacer llamadas similares al cargar, borrar vaciar transacciones para actualizar saldos a confirmar.

---
+++ BlockChainHistoryBook.h
@@ -0,0 +1,35 @@
+/*
+ * BlockChainHistoryBook.h
+ *
+ *  Created on: 30 nov. 2020
+ *      Author: ramiro
+ */
+
+#ifndef BLOCKCHAINHISTORYBOOK_H_
+#define BLOCKCHAINHISTORYBOOK_H_
+
+#include <string>
+#include "lista.h"
+#include "Block.h"
+#include "Cuentas.h"
+
+class BlockChainHistoryBook {
+private:
+	friend class BlockChainBookkeeper;
+	static lista<Block*> AlgoChain;
+	static Cuentas ListadoCuentas;
+	//---Getters---//
+	static const lista <Block *> & getListaBlocks(){return AlgoChain;};
+	// Para usar x línea de comandos block <id>
+	static const Block * getBlock( const std::string txns_hash );						// Ante cualquier error devuelve NULL
+	// Para usar x línea de comandos txn <id>
+	static const TransactionInput * obtenerTransactionInput( const std::string tx_id );	// Ante cualquier error devuelve NULL
+	//---Setters---//
+	//---Otros---//
+	static void BorrarHistoria( void );
+	static bool AddBlock( Block *& B );
+	static bool AddListaBlocks( lista <Block *> & lista );
+public:
+};
+
+#endif /* BLOCKCHAINHISTORYBOOK_H_ */
